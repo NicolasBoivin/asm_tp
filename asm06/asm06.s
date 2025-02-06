458214446a2f36d54b8322fd976065c7c78ddde7
NicolasBoivin <nicolas.boivin@ynov.com>
2025-02-06 10:26:09 +0100

    W

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -5,41 +5,50 @@ section .text
     global _start
 
 _start:
-    pop rcx        ; nombre d'arguments
-    cmp rcx, 3     ; vérifier si on a 2 arguments
+    pop rcx         ; nombre d'arguments
+    cmp rcx, 3      ; vérifier si on a 2 arguments
     jne exit_error
 
-    pop rcx        ; ignorer le nom du programme
-    pop rsi        ; premier nombre
-    call atoi      ; convertir en entier
-    push rax       ; sauvegarder le résultat
+    pop rcx         ; ignorer le nom du programme
+    pop rsi         ; premier nombre
+    call atoi      
+    push rax       
 
-    pop rsi        ; deuxième nombre
-    call atoi
-    pop rdi        ; récupérer le premier nombre
-    add rax, rdi   ; addition
+    pop rsi         ; deuxième nombre
+    call atoi      
+    push rax       
 
-    mov rdi, rax   ; préparer pour itoa
-    mov rsi, result
+    pop rcx         ; second nombre
+    pop rbx         ; premier nombre
+    add rbx, rcx    ; addition
+
+    mov rdi, rbx    ; préparer pour itoa
     call itoa
 
-    mov rax, 1     ; afficher le résultat
-    mov rdi, 1
+    mov rdi, 1      ; afficher le résultat
     mov rsi, result
     mov rdx, rax
+    mov rax, 1
     syscall
 
-    mov rax, 60    ; exit success
-    xor rdi, rdi
+    xor rdi, rdi    ; exit success
+    mov rax, 60
     syscall
 
 exit_error:
-    mov rax, 60
     mov rdi, 1
+    mov rax, 60
     syscall
 
 atoi:
-    xor rax, rax
+    xor rax, rax    ; initialiser le résultat
+    xor rbx, rbx    ; signe (0 = positif)
+    
+    cmp byte [rsi], '-'
+    jne .loop
+    mov rbx, 1      ; nombre négatif
+    inc rsi
+    
 .loop:
     movzx rcx, byte [rsi]
     test rcx, rcx
@@ -53,16 +62,31 @@ atoi:
     add rax, rcx
     inc rsi
     jmp .loop
+    
 .end:
+    test rbx, rbx
+    jz .positive
+    neg rax         ; si négatif, inverser le signe
+.positive:
     ret
 
 itoa:
-    push rbx
-    mov rbx, 10
+    mov rax, rdi
     mov rcx, result
     add rcx, 19
-    mov byte [rcx], 10
-    mov rax, rdi
+    mov byte [rcx], 10  ; newline
+    
+    test rax, rax
+    jns .positive
+    
+    neg rax         ; si négatif, rendre positif
+    push rax
+    mov byte [result], '-'
+    pop rax
+    
+.positive:
+    mov rbx, 10
+    
 .loop:
     dec rcx
     xor rdx, rdx
@@ -71,9 +95,15 @@ itoa:
     mov [rcx], dl
     test rax, rax
     jnz .loop
+    
+    test rdi, rdi   ; vérifier si le nombre était négatif
+    jns .end
+    dec rcx
+    mov byte [rcx], '-'
+    
+.end:
     mov rax, result
     add rax, 20
     sub rax, rcx
     mov rsi, rcx
-    pop rbx
     ret
