b863ad9953a415309ec0941fbfbe0666f224f015
NicolasBoivin <nicolas.boivin@ynov.com>
2025-02-06 09:23:04 +0100

    W

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -5,26 +5,34 @@ section .text
     global _start
 
 _start:
-    mov rdi, [rsp+8]   
-    test rdi, rdi      
-    jz exit_fail       
+    ; Vérifier si un argument est passé
+    mov rdi, [rsp+8]         ; Le premier argument passé en paramètre
+    test rdi, rdi
+    jz exit_fail             ; Si aucun argument, sortir avec code 1
 
-    mov rsi, [rdi]     
-    mov eax, dword [rsi] 
-    cmp eax, '42'       
-    jne exit_fail       
+    ; Comparer l'argument avec "42"
+    mov rsi, rdi
+    movzx rdi, byte [rsi]    ; Charger le premier caractère de l'argument
+    cmp rdi, '4'
+    jne exit_fail            ; Si ce n'est pas "4", sortir avec code 1
+    movzx rdi, byte [rsi+1]  ; Charger le deuxième caractère
+    cmp rdi, '2'
+    jne exit_fail            ; Si ce n'est pas "2", sortir avec code 1
 
-    mov rax, 1         
-    mov rdi, 1         
-    mov rsi, message   
-    mov rdx, 4         
-    syscall            
+    ; Afficher "1337"
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, message
+    mov rdx, 4
+    syscall
 
-    mov rax, 60        
-    xor rdi, rdi       
-    syscall            
+    ; Sortie avec code 0
+    mov rax, 60
+    xor rdi, rdi
+    syscall
 
 exit_fail:
-    mov rax, 60        
-    mov rdi, 1         
-    syscall            
+    ; Sortie avec code 1
+    mov rax, 60
+    mov rdi, 1
+    syscall
