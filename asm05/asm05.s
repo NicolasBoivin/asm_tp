cd0883ac680a10a8109734f2342010432531c375
NicolasBoivin <nicolas.boivin@ynov.com>
2025-02-06 10:09:51 +0100

    W

--- asm05/asm05.s
+++ asm05/asm05.s
@@ -2,6 +2,9 @@ section .text
     global _start
 
 _start:
+    cmp rdi, 1
+    je no_param
+
     mov rdi, [rsp + 8]
     mov rax, 1
     mov rsi, rdi
@@ -10,4 +13,9 @@ _start:
 
     mov rax, 60
     xor rdi, rdi
+    syscall
+
+no_param:
+    mov rax, 60
+    mov rdi, 1
     syscall
