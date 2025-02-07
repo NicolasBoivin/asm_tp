a5f518983a7f158d74c2b7e043ec669ba53ffe58
NicolasBoivin <nicolas.boivin@ynov.com>
2025-02-07 12:35:37 +0100

    W

--- asm17/asm17.s
+++ asm17/asm17.s
@@ -1,52 +1,102 @@
 section .bss
-    buffer resb 256
+    input resb 1024
+    shift resb 1
 
 section .text
-    global _start
-    extern atoi
+global _start
 
 _start:
-    mov rdi, [rsp+16]
+    pop rcx
+    cmp rcx, 2
+    jne error
+    pop rcx
+    pop rdi
+    xor rax, rax
+    mov rsi, shift
     call atoi
-    mov r12d, eax
 
-    mov rax, 0
-    mov rdi, 0
-    mov rsi, buffer
-    mov rdx, 256
+read_input:
+    xor rax, rax
+    xor rdi, rdi
+    mov rsi, input
+    mov rdx, 1024
     syscall
-    mov r13, rax
 
-    mov rbx, 0
-encode_loop:
-    cmp rbx, r13
-    jge end_encode
+    mov rcx, rax
+    mov rsi, input
+    xor rbx, rbx
 
-    mov al, [buffer+rbx]
+process_char:
+    cmp rbx, rcx
+    jge write_output
+
+    mov al, [rsi + rbx]
+
+    cmp al, 'A'
+    jl next_char
+    cmp al, 'Z'
+    jle upper_case
     cmp al, 'a'
-    jb skip
+    jl next_char
     cmp al, 'z'
-    ja skip
+    jg next_char
 
-    add al, r12b
+    mov dl, [shift]
+    add al, dl
     cmp al, 'z'
-    jle store
+    jle store_char
+    sub al, 26
+    jmp store_char
+
+upper_case:
+    mov dl, [shift]
+    add al, dl
+    cmp al, 'Z'
+    jle store_char
     sub al, 26
 
-store:
-    mov [buffer+rbx], al
+store_char:
+    mov [rsi + rbx], al
 
-skip:
+next_char:
     inc rbx
-    jmp encode_loop
+    jmp process_char
 
-end_encode:
+write_output:
     mov rax, 1
     mov rdi, 1
-    mov rsi, buffer
-    mov rdx, r13
+    mov rdx, rcx
+    mov rsi, input
     syscall
 
-    mov rax, 60
     xor rdi, rdi
+    jmp exit
+
+error:
+    mov rax, 60
+    mov rdi, 1
     syscall
+
+exit:
+    mov rax, 60
+    mov rdi, rdi
+    syscall
+
+atoi:
+    xor rax, rax
+convert_loop:
+    movzx rcx, byte [rdi]
+    cmp rcx, 0
+    je atoi_end
+    cmp rcx, '0'
+    jl atoi_end
+    cmp rcx, '9'
+    jg atoi_end
+    sub rcx, '0'
+    imul rax, 10
+    add rax, rcx
+    inc rdi
+    jmp convert_loop
+atoi_end:
+    mov [rsi], al
+    ret
