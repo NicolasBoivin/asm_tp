cdb64f086e9ef6cb9eb8e8c6a023ec9d603fcb07
NicolasBoivin <nicolas.boivin@ynov.com>
2025-02-07 12:14:28 +0100

    W

--- asm14/asm14.s
+++ asm14/asm14.s
@@ -2,9 +2,6 @@ section .data
     message db "Hello Universe!", 10
     message_len equ $ - message
 
-section .bss
-    filename resb 256   ; Stocke le nom du fichier
-
 section .text
 global _start
 
@@ -19,33 +16,34 @@ _start:
     pop rsi  ; Nom du fichier
 
     ; Ouvrir le fichier en mode écriture/création
-    mov rax, 257        ; syscall: openat
-    mov rdi, -100       ; AT_FDCWD (répertoire de travail actuel)
-    mov rdx, 0x601      ; O_WRONLY | O_CREAT | O_TRUNC
-    mov r10, 0644       ; Permissions rw-r--r--
+    mov rax, 2        ; syscall: open
+    mov rdi, rsi      ; Nom du fichier
+    mov rsi, 0x601    ; O_WRONLY | O_CREAT | O_TRUNC
+    mov rdx, 0644o    ; Permissions rw-r--r--
     syscall
     cmp rax, 0
     js exit_error
-    mov rdi, rax        ; Sauvegarde du descripteur de fichier
+    mov rdi, rax      ; Sauvegarde du descripteur de fichier
 
     ; Écrire le message dans le fichier
-    mov rax, 1          ; syscall: write
-    mov rsi, message    ; Adresse du message
-    mov rdx, message_len ; Longueur du message
+    mov rax, 1        ; syscall: write
+    mov rsi, message  ; Adresse du message
+    mov rdx, message_len  ; Longueur du message
     syscall
     cmp rax, 0
     js exit_error
 
     ; Fermer le fichier
-    mov rax, 3          ; syscall: close
+    mov rax, 3        ; syscall: close
+    mov rdi, rdi      ; Fermer le fichier ouvert
     syscall
 
     ; Sortie avec succès
-    mov rax, 60         ; syscall: exit
-    xor rdi, rdi        ; code de retour 0
+    mov rax, 60      ; syscall: exit
+    xor rdi, rdi     ; code de retour 0
     syscall
 
 exit_error:
-    mov rax, 60         ; syscall: exit
-    mov rdi, 1          ; code de retour 1
+    mov rax, 60      ; syscall: exit
+    mov rdi, 1       ; code de retour 1
     syscall
