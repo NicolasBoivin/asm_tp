87f0df9f42952c9e623e8d17a7453a984a214b13
NicolasBoivin <nicolas.boivin@ynov.com>
2025-02-07 12:13:14 +0100

    W

--- asm14/asm14.s
+++ asm14/asm14.s
@@ -10,40 +10,42 @@ global _start
 
 _start:
     ; Vérifier le nombre d'arguments
-    pop rcx
-    cmp rcx, 2
+    pop rdi
+    cmp rdi, 2
     jne exit_error
 
     ; Récupérer le nom du fichier
-    pop rsi
     pop rsi  ; Ignorer le premier argument (nom du programme)
+    pop rsi  ; Nom du fichier
 
     ; Ouvrir le fichier en mode écriture/création
-    mov rax, 2        ; syscall: open
-    mov rdi, rsi      ; Nom du fichier
-    mov rsi, 0x601    ; O_WRONLY | O_CREAT | O_TRUNC
-    mov rdx, 0644o    ; Permissions rw-r--r--
+    mov rax, 257        ; syscall: openat
+    mov rdi, -100       ; AT_FDCWD (répertoire de travail actuel)
+    mov rdx, 0x601      ; O_WRONLY | O_CREAT | O_TRUNC
+    mov r10, 0644       ; Permissions rw-r--r--
     syscall
     cmp rax, 0
-    jl exit_error
-    mov rdi, rax      ; Sauvegarde du descripteur de fichier
+    js exit_error
+    mov rdi, rax        ; Sauvegarde du descripteur de fichier
 
     ; Écrire le message dans le fichier
-    mov rax, 1        ; syscall: write
-    mov rsi, message  ; Adresse du message
-    mov rdx, message_len  ; Longueur du message
+    mov rax, 1          ; syscall: write
+    mov rsi, message    ; Adresse du message
+    mov rdx, message_len ; Longueur du message
     syscall
+    cmp rax, 0
+    js exit_error
 
     ; Fermer le fichier
-    mov rax, 3        ; syscall: close
+    mov rax, 3          ; syscall: close
     syscall
 
     ; Sortie avec succès
-    mov rax, 60      ; syscall: exit
-    xor rdi, rdi     ; code de retour 0
+    mov rax, 60         ; syscall: exit
+    xor rdi, rdi        ; code de retour 0
     syscall
 
 exit_error:
-    mov rax, 60      ; syscall: exit
-    mov rdi, 1       ; code de retour 1
+    mov rax, 60         ; syscall: exit
+    mov rdi, 1          ; code de retour 1
     syscall
