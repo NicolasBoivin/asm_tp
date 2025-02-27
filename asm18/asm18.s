292ba75ca120ac7136d7c0997dd50cc60ac10e2b
NicolasBoivin <nicolas.boivin@ynov.com>
2025-02-27 12:14:09 +0100

    modif

--- asm18/asm18.s
+++ asm18/asm18.s
@@ -1,128 +1,117 @@
-.section .data
-msg_prefix:
+ .section .data
+prefix:
     .ascii "message: \""
-msg_prefix_len = . - msg_prefix
+prefix_len = . - prefix
 
-msg_suffix:
+suffix:
     .ascii "\"\n"
-msg_suffix_len = . - msg_suffix
+suffix_len = . - suffix
 
-err_timeout:
+timeout_msg:
     .ascii "Timeout: no response from server\n"
-err_timeout_len = . - err_timeout
+timeout_msg_len = . - timeout_msg
 
-request_data:
+query:
     .ascii "ping"
-request_len = . - request_data
+query_len = . - query
 
-server_addr:
-    .word 2                  # AF_INET
-    .word 0x3905             # Port 1337 (network byte order)
-    .long 0x0100007f         # 127.0.0.1 (network byte order)
-    .quad 0                  # Padding
+dest:
+    .word 2
+    .word 0x3905
+    .long 0x0100007f
+    .quad 0
 
-wait_time:
-    .quad 1                  # 1 second
-    .quad 0                  # 0 nanoseconds
+timeval:
+    .quad 1
+    .quad 0
 
     .section .bss
-    .lcomm buffer, 256       # Buffer pour la réponse
+    .lcomm recvbuf,256
 
     .section .text
     .global _start
 _start:
-    # Créer socket UDP
-    mov     $41, %rax        # socket syscall
-    mov     $2, %rdi         # AF_INET
-    mov     $2, %rsi         # SOCK_DGRAM
-    xor     %rdx, %rdx       # Protocol = 0
+    movq    $41, %rax
+    movq    $2, %rdi
+    movq    $2, %rsi
+    xor    %rdx, %rdx
     syscall
-    cmp     $0, %rax
-    jl      error_exit
-    mov     %rax, %rbx       # Sauvegarder le descripteur dans %rbx
+    cmpq    $0, %rax
+    js    exit_fail
+    movq    %rax, %r12
 
-    # Configurer le timeout
-    mov     %rbx, %rdi       # Socket FD
-    mov     $1, %rsi         # SOL_SOCKET
-    mov     $20, %rdx        # SO_RCVTIMEO
-    lea     wait_time(%rip), %r10
-    mov     $16, %r8         # Taille de timeval
-    mov     $54, %rax        # setsockopt syscall
-    syscall
-
-    # Envoyer données
-    mov     %rbx, %rdi       # Socket FD
-    lea     request_data(%rip), %rsi
-    mov     $request_len, %rdx
-    xor     %r10, %r10       # Flags = 0
-    lea     server_addr(%rip), %r8
-    mov     $16, %r9         # Taille de l'adresse
-    mov     $44, %rax        # sendto syscall
+    movq    %r12, %rdi
+    movq    $1, %rsi
+    movq    $20, %rdx
+    leaq    timeval(%rip), %r10
+    movq    %r10, %r10
+    movq    $16, %r8
+    movq    $54, %rax
     syscall
 
-    # Recevoir la réponse
-    mov     %rbx, %rdi       # Socket FD
-    lea     buffer(%rip), %rsi
-    mov     $256, %rdx       # Taille max du buffer
-    xor     %r10, %r10       # Flags = 0
-    xor     %r8, %r8         # Adresse source NULL
-    xor     %r9, %r9         # Taille adresse source = 0
-    mov     $45, %rax        # recvfrom syscall
-    syscall
-    cmp     $0, %rax
-    jl      handle_timeout
-    mov     %rax, %r12       # Sauvegarder la taille reçue
-
-    # Afficher le préfixe
-    mov     $1, %rdi         # STDOUT
-    lea     msg_prefix(%rip), %rsi
-    mov     $msg_prefix_len, %rdx
-    mov     $1, %rax         # write syscall
+    movq    %r12, %rdi
+    leaq    query(%rip), %rsi
+    movq    $query_len, %rdx
+    xor    %r10, %r10
+    leaq    dest(%rip), %r8
+    movq    $16, %r9
+    movq    $44, %rax
     syscall
 
-    # Afficher la réponse
-    mov     $1, %rdi         # STDOUT
-    lea     buffer(%rip), %rsi
-    mov     %r12, %rdx       # Taille de la réponse
-    mov     $1, %rax         # write syscall
+    movq    %r12, %rdi
+    leaq    recvbuf(%rip), %rsi
+    movq    $256, %rdx
+    xor    %r10, %r10
+    xor    %r8, %r8
+    xor    %r9, %r9
+    movq    $45, %rax
+    syscall
+    cmpq    $0, %rax
+    js    timeout_label
+    movq    %rax, %r13
+
+    movq    $1, %rdi
+    leaq    prefix(%rip), %rsi
+    movq    $prefix_len, %rdx
+    movq    $1, %rax
     syscall
 
-    # Afficher le suffixe
-    mov     $1, %rdi         # STDOUT
-    lea     msg_suffix(%rip), %rsi
-    mov     $msg_suffix_len, %rdx
-    mov     $1, %rax         # write syscall
+    movq    $1, %rdi
+    leaq    recvbuf(%rip), %rsi
+    movq    %r13, %rdx
+    movq    $1, %rax
     syscall
 
-    # Fermer le socket
-    mov     %rbx, %rdi
-    mov     $3, %rax         # close syscall
+    movq    $1, %rdi
+    leaq    suffix(%rip), %rsi
+    movq    $suffix_len, %rdx
+    movq    $1, %rax
     syscall
 
-    # Exit avec code 0
-    mov     $60, %rax        # exit syscall
-    xor     %rdi, %rdi       # Code 0
+    movq    %r12, %rdi
+    movq    $3, %rax
     syscall
 
-handle_timeout:
-    # Afficher message timeout
-    mov     $1, %rdi         # STDOUT
-    lea     err_timeout(%rip), %rsi
-    mov     $err_timeout_len, %rdx
-    mov     $1, %rax         # write syscall
+    movq    $60, %rax
+    xor    %rdi, %rdi
     syscall
 
-    # Fermer le socket
-    mov     %rbx, %rdi
-    mov     $3, %rax         # close syscall
+timeout_label:
+    movq    $1, %rdi
+    leaq    timeout_msg(%rip), %rsi
+    movq    $timeout_msg_len, %rdx
+    movq    $1, %rax
     syscall
 
-    # Exit avec code 1
-    mov     $60, %rax        # exit syscall
-    mov     $1, %rdi         # Code 1
+    movq    %r12, %rdi
+    movq    $3, %rax
     syscall
 
-error_exit:
-    mov     $60, %rax        # exit syscall
-    mov     $1, %rdi         # Code 1
+    movq    $60, %rax
+    movq    $1, %rdi
+    syscall
+
+exit_fail:
+    movq    $60, %rax
+    movq    $1, %rdi
     syscall
